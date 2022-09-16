e0b92868dd67cad13bf0329b779102b6b6ed6148
Arrikk <horpeyhermi@gmail.com>
2022-09-16 12:31:54 +0100

    fixedGarbled

--- App/Views/components/template/ui-post-single.php
+++ App/Views/components/template/ui-post-single.php
@@ -62,7 +62,7 @@ function __ui_single_body($data)
     <div class="ui-post-content">
         <h2><?= html_entity_decode($data->post_title) ?></h2>
         <div class="ui-post-article">
-            <?= html_entity_decode($data->post_body) ?>
+            <?= html_entity_decode(htmlspecialchars_decode($data->post_body)) ?>
         </div>
     </div>
 <?php
